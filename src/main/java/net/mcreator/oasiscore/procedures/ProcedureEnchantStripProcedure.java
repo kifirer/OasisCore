c21383ae623c3138fff8e404c6dc1697b2f63d79
kifirer <user@DESKTOP-0HLCBJP>
2021-05-16 17:37:27 +0300

    added exp 'strip' enchantment

---
+++ src/main/java/net/mcreator/oasiscore/procedures/ProcedureEnchantStripProcedure.java
@@ -0,0 +1,177 @@
+package net.mcreator.oasiscore.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.oasiscore.enchantment.EnchantStripEnchantment;
+import net.mcreator.oasiscore.OasiscoreModElements;
+import net.mcreator.oasiscore.OasiscoreMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@OasiscoreModElements.ModElement.Tag
+public class ProcedureEnchantStripProcedure extends OasiscoreModElements.ModElement {
+	public ProcedureEnchantStripProcedure(OasiscoreModElements instance) {
+		super(instance, 100);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				OasiscoreMod.LOGGER.warn("Failed to load dependency entity for procedure ProcedureEnchantStrip!");
+			return;
+		}
+		if (dependencies.get("imediatesourceentity") == null) {
+			if (!dependencies.containsKey("imediatesourceentity"))
+				OasiscoreMod.LOGGER.warn("Failed to load dependency imediatesourceentity for procedure ProcedureEnchantStrip!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				OasiscoreMod.LOGGER.warn("Failed to load dependency x for procedure ProcedureEnchantStrip!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				OasiscoreMod.LOGGER.warn("Failed to load dependency y for procedure ProcedureEnchantStrip!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				OasiscoreMod.LOGGER.warn("Failed to load dependency z for procedure ProcedureEnchantStrip!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				OasiscoreMod.LOGGER.warn("Failed to load dependency world for procedure ProcedureEnchantStrip!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		Entity imediatesourceentity = (Entity) dependencies.get("imediatesourceentity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((((EnchantmentHelper.getEnchantmentLevel(EnchantStripEnchantment.enchantment,
+				((imediatesourceentity instanceof LivingEntity)
+						? ((LivingEntity) imediatesourceentity).getHeldItemOffhand()
+						: ItemStack.EMPTY)) != 0))
+				|| ((EnchantmentHelper.getEnchantmentLevel(EnchantStripEnchantment.enchantment,
+						((imediatesourceentity instanceof LivingEntity)
+								? ((LivingEntity) imediatesourceentity).getHeldItemMainhand()
+								: ItemStack.EMPTY)) != 0)))) {
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z,
+						((entity instanceof LivingEntity)
+								? ((LivingEntity) entity)
+										.getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 0))
+								: ItemStack.EMPTY));
+				entityToSpawn.setPickupDelay((int) 10);
+				world.addEntity(entityToSpawn);
+			}
+			if (entity instanceof LivingEntity) {
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.armorInventory.set((int) 0, new ItemStack(Blocks.AIR, (int) (1)));
+				else
+					((LivingEntity) entity).setItemStackToSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 0),
+							new ItemStack(Blocks.AIR, (int) (1)));
+				if (entity instanceof ServerPlayerEntity)
+					((ServerPlayerEntity) entity).inventory.markDirty();
+			}
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z,
+						((entity instanceof LivingEntity)
+								? ((LivingEntity) entity)
+										.getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 1))
+								: ItemStack.EMPTY));
+				entityToSpawn.setPickupDelay((int) 10);
+				world.addEntity(entityToSpawn);
+			}
+			if (entity instanceof LivingEntity) {
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.armorInventory.set((int) 1, new ItemStack(Blocks.AIR, (int) (1)));
+				else
+					((LivingEntity) entity).setItemStackToSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 1),
+							new ItemStack(Blocks.AIR, (int) (1)));
+				if (entity instanceof ServerPlayerEntity)
+					((ServerPlayerEntity) entity).inventory.markDirty();
+			}
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z,
+						((entity instanceof LivingEntity)
+								? ((LivingEntity) entity)
+										.getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 2))
+								: ItemStack.EMPTY));
+				entityToSpawn.setPickupDelay((int) 10);
+				world.addEntity(entityToSpawn);
+			}
+			if (entity instanceof LivingEntity) {
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.armorInventory.set((int) 2, new ItemStack(Blocks.AIR, (int) (1)));
+				else
+					((LivingEntity) entity).setItemStackToSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 2),
+							new ItemStack(Blocks.AIR, (int) (1)));
+				if (entity instanceof ServerPlayerEntity)
+					((ServerPlayerEntity) entity).inventory.markDirty();
+			}
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z,
+						((entity instanceof LivingEntity)
+								? ((LivingEntity) entity)
+										.getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 3))
+								: ItemStack.EMPTY));
+				entityToSpawn.setPickupDelay((int) 10);
+				world.addEntity(entityToSpawn);
+			}
+			if (entity instanceof LivingEntity) {
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.armorInventory.set((int) 3, new ItemStack(Blocks.AIR, (int) (1)));
+				else
+					((LivingEntity) entity).setItemStackToSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 3),
+							new ItemStack(Blocks.AIR, (int) (1)));
+				if (entity instanceof ServerPlayerEntity)
+					((ServerPlayerEntity) entity).inventory.markDirty();
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityAttacked(LivingAttackEvent event) {
+		if (event != null && event.getEntity() != null) {
+			Entity entity = event.getEntity();
+			Entity sourceentity = event.getSource().getTrueSource();
+			Entity imediatesourceentity = event.getSource().getImmediateSource();
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			double amount = event.getAmount();
+			World world = entity.world;
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("amount", amount);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("sourceentity", sourceentity);
+			dependencies.put("imediatesourceentity", imediatesourceentity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
